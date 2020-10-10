80990e8cde3b17977ad63e45075a48bb84971c36
Leung <2719791520@qq.com>
2020-10-10 23:38:51 +0800

    新增程序入口

---
+++ SEPairProject/src/main/java/com/czl/exercisesgeneration/service/myImpl/ProjectEntry.java
@@ -0,0 +1,7 @@
+package com.czl.exercisesgeneration.service.myImpl;
+
+public class ProjectEntry {
+    public static void main(String[] args) throws Exception {
+        UserInput.menu(args);
+    }
+}
